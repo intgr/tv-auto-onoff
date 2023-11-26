b55a84ef7c47862af4f8be2a24503456cf371d03
Marti Raudsepp <marti@juffo.org>
2023-11-26 20:58:12 +0200

    Setup logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use log::{debug, LevelFilter};
+use simple_logger::SimpleLogger;
+use time::macros::format_description;
+
 fn main() {
-    println!("Hello, world!");
+    SimpleLogger::new()
+        .with_local_timestamps()
+        .with_timestamp_format(format_description!(
+            "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]"
+        ))
+        .with_level(LevelFilter::Debug)
+        .with_colors(true)
+        .init()
+        .unwrap();
+
+    debug!("{} {} starting...", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 }
