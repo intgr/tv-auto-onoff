b1bb8254706be45c23639b2d0008aec438eccb7c
Marti Raudsepp <marti@juffo.org>
2023-12-12 02:00:45 +0200

    Omit log timestamps and colors when under systemd

--- src/logging.rs
+++ src/logging.rs
@@ -1,21 +1,52 @@
+use std::io::stdout;
+use std::os::fd::AsRawFd;
+use std::{env, mem};
+
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
 use time::macros::format_description;
 use time::util::local_offset;
 use time::util::local_offset::Soundness;
 
+/// Is current process directly connected to the systemd journal?
+/// Somewhat inspired by `systemd-journal-logger`.
+#[cfg(target_os = "linux")]
+pub fn connected_to_journal() -> bool {
+    let Some(journal_stream) = env::var_os("JOURNAL_STREAM") else {
+        return false;
+    };
+    let mut stat: libc::stat = unsafe { mem::zeroed() };
+    let result = unsafe { libc::fstat(stdout().as_raw_fd(), &mut stat) };
+    if result != 0 {
+        return false;
+    }
+    return journal_stream.to_string_lossy() == format!("{}:{}", stat.st_dev, stat.st_ino);
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn connected_to_journal() -> bool {
+    false
+}
+
 #[allow(clippy::module_name_repetitions)]
 pub fn init_logging() {
-    // time-rs is silly...
-    unsafe { local_offset::set_soundness(Soundness::Unsound) };
+    // Omit log timestamps and colors when running under systemd.
+    let builder = if connected_to_journal() {
+        SimpleLogger::new().without_timestamps()
+    } else {
+        // time-rs is silly...
+        unsafe { local_offset::set_soundness(Soundness::Unsound) };
 
-    SimpleLogger::new()
-        .with_local_timestamps()
-        .with_timestamp_format(format_description!(
-            "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]"
-        ))
+        SimpleLogger::new()
+            .with_local_timestamps()
+            .with_timestamp_format(format_description!(
+                "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]"
+            ))
+            .with_colors(true)
+    };
+
+    builder
         .with_level(LevelFilter::Debug)
-        .with_colors(true)
         .init()
         .expect("Error initializing logging");
 }
